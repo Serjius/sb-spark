011021617357897f2ce3bcfed6487452825a24e5
Serge Puchnin <Sergey.Puchnin@gmail.com>
2020-06-04 21:41:39 +0300

    assembly and build script

---
+++ l03.sh
@@ -0,0 +1,7 @@
+cd lab03/data_mart
+#sbt package
+sbt clean assembly
+#scp .target/scala-2.11/data_mart_2.11-1.0.jar sdm:lab03/.
+scp .target/scala-2.11/data_mart-assembly-1.0.jar sdm:lab03/.
+scp resources/log4j2.xml sdm:lab03/resources/.
+cd ../..
